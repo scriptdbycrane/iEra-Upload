cd435a837c94844044c8c4db2923b0b7758f6951
Crane <scriptdbycrane@outlook.com>
2022-08-22 12:19:37 -1000

    Add custom JavaScript execution safeguard for upcoming releases

--- iEra Upload/Form1.cs
+++ iEra Upload/Form1.cs
@@ -5,8 +5,8 @@ namespace iEra_Upload
 {
     public partial class Form1 : Form
     {
-        private CefSettings? Settings = null;
-        private ChromiumWebBrowser? WebBrowser = null;
+        private ChromiumWebBrowser WebBrowser = new ChromiumWebBrowser();
+        private System.Timers.Timer JSTimer = new System.Timers.Timer();
 
         public Form1()
         {
@@ -16,19 +16,41 @@ namespace iEra_Upload
 
         private void InitializeChromium()
         {
-            this.Settings = new CefSettings();
-            Cef.Initialize(this.Settings);
+            // Initialize the settings.
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
 
+            // Confiugure the web browser.
             const String URL = "https://eraupload.graalonline.com";
-            this.WebBrowser = new ChromiumWebBrowser(URL);
+            this.WebBrowser.Load(URL);
             this.Controls.Add(this.WebBrowser);
 
             this.WebBrowser.Dock = DockStyle.Fill;
             this.WebBrowser.Show();
+
+            // This timer determines when any custom JavaScript should begin executing in the web browser.
+            this.JSTimer = new System.Timers.Timer();
+            this.JSTimer.Interval = 500;
+            this.JSTimer.Elapsed += this.WebBrowserTimer_Elapsed;
+            this.JSTimer.Start();
+        }
+
+        private void WebBrowserTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                // Attempting to run any custom JavaScript before the web browser is initialized will result in a crash.
+                if (this.WebBrowser.IsBrowserInitialized)
+                {
+                    this.JSTimer.Stop();
+                    this.JSTimer.Dispose();
+                }
+            }));
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Set the permanent dimensions of the application.
             this.MinimumSize = new Size(this.Width, this.Height);
             this.MaximumSize = this.MinimumSize;
         }
