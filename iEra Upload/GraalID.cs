5c4839cd05898d7cdd4df0d87837cc947e077abc
Crane <scriptdbycrane@outlook.com>
2022-08-23 17:51:00 -1000

    Remove unused namespaces

--- iEra Upload/GraalID.cs
+++ iEra Upload/GraalID.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace iEra_Upload
+﻿namespace iEra_Upload
 {
     internal class GraalID
     {
