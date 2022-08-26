5058bffd769beb517b28319ce0e15d1a0b6691c9
Crane <scriptdbycrane@outlook.com>
2022-08-25 16:59:03 -1000

    Initial commit

---
+++ iEra Upload/Resource.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Policy;
+
+namespace iEra_Upload
+{
+    internal class Resource
+    {
+        public string Path { get; set; }
+
+        public Resource(string path)
+        {
+            Path = path;
+        }
+
+        public bool Exists()
+        {
+            return Directory.Exists(Path) || File.Exists(Path);
+        }
+    }
+}
