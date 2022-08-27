e5c4ce8ca9dbd36e145307f27014f69e7a0dc6af
Crane <scriptdbycrane@outlook.com>
2022-08-26 18:06:24 -1000

    Organize source tree

---
+++ iEra Upload/src/App.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace iEra_Upload
+{
+    partial class App
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Location = new System.Drawing.Point(12, 12);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(75, 25);
+            this.RefreshButton.TabIndex = 0;
+            this.RefreshButton.TabStop = false;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Visible = false;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 597);
+            this.Controls.Add(this.RefreshButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "App";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "iEra Upload";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.App_FormClosing);
+            this.Load += new System.EventHandler(this.App_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button RefreshButton;
+    }
+}
---
+++ iEra Upload/src/Resource.cs
@@ -0,0 +1,32 @@
+﻿namespace iEra_Upload
+{
+    internal class Resource
+    {
+        public enum ResourceType
+        {
+            Directory,
+            File
+        };
+        public ResourceType Type { get; set; }
+        public string Path { get; set; }
+
+        public Resource(ResourceType type, string path)
+        {
+            Type = type;
+            Path = path;
+        }
+
+        public bool Exists()
+        {
+            switch (Type)
+            {
+                case ResourceType.Directory:
+                    return Directory.Exists(Path);
+                case ResourceType.File:
+                    return File.Exists(Path);
+                default:
+                    return false;
+            }
+        }
+    }
+}
